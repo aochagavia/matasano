d6de0c887ffd6a65cdbadc8f8c392c55cf9b5422
Adolfo Ochagavía <github@adolfo.ochagavia.xyz>
2019-09-23 20:25:53 +0200

    Challenge 11

--- src/aes.rs
+++ src/aes.rs
@@ -17,6 +17,11 @@ pub fn decrypt_aes_ecb_no_padding(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
     plaintext
 }
 
+pub fn encrypt_aes_ecb(plaintext: &[u8], key: &[u8]) -> Vec<u8> {
+    let cipher = Cipher::aes_128_ecb();
+    encrypt(cipher, key, None, plaintext).unwrap()
+}
+
 pub fn encrypt_aes_ecb_no_padding(plaintext: &[u8], key: &[u8]) -> Vec<u8> {
     let cipher = Cipher::aes_128_ecb();
     let mut crypter = Crypter::new(cipher, Mode::Encrypt, key, None).unwrap();
@@ -83,6 +88,58 @@ pub fn count_repetitions(bytes: &[u8]) -> u32 {
     freq.iter().filter(|&(_, &v)| v > 1).map(|(_, v)| v).sum()
 }
 
+pub fn aes_oracle() -> (BlockCipherMode, BlockCipherMode) {
+    let plaintext = vec![b'A'; 16 * 6];
+    let (ciphertext, mode) = encrypt_aes_cbc_or_ecb(&plaintext);
+
+    // If the encryption function is using ECB, there will be at least
+    // 4 repeated blocks
+    if count_repetitions(&ciphertext) >= 4 {
+        (BlockCipherMode::ECB, mode)
+    } else {
+        (BlockCipherMode::CBC, mode)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum BlockCipherMode {
+    ECB,
+    CBC
+}
+
+fn encrypt_aes_cbc_or_ecb(plaintext: &[u8]) -> (Vec<u8>, BlockCipherMode) {
+    use rand::{RngCore, Rng};
+
+    // Generate random key
+    let mut key = vec![0; 16];
+    let mut rng = rand::thread_rng();
+    rng.fill_bytes(&mut key);
+
+    // Modify the plaintext
+    let prepend_count: usize = rng.gen_range(5, 11);
+    let mut prepend_bytes = vec![0; prepend_count];
+    rng.fill_bytes(&mut prepend_bytes);
+
+    let append_count: usize = rng.gen_range(5, 11);
+    let mut append_bytes = vec![0; append_count];
+    rng.fill_bytes(&mut append_bytes);
+
+    let mut new_plaintext = prepend_bytes;
+    new_plaintext.extend_from_slice(plaintext);
+    new_plaintext.extend_from_slice(&append_bytes);
+
+    // Encrypt!
+    if rng.gen_bool(0.5) {
+        // CBC
+        let mut iv = vec![0; 16];
+        rng.fill_bytes(&mut iv);
+        (encrypt_aes_cbc(&new_plaintext, &key, &iv), BlockCipherMode::CBC)
+    } else {
+        // ECB
+        (encrypt_aes_ecb(&new_plaintext, &key), BlockCipherMode::ECB)
+    }
+}
+
 #[test]
 fn test_set_1_challenge_8() {
     use std::fs::File;
@@ -124,4 +181,12 @@ fn test_encrypt_decrypt_aes_cbc() {
     let decrypted = decrypt_aes_cbc(&ciphertext, key, iv);
 
     assert_eq!(decrypted, plaintext);
+}
+
+#[test]
+fn test_detect_aes_ecb() {
+    for _ in 0..20 {
+        let (detected, real) = aes_oracle();
+        assert_eq!(detected, real);
+    }
 }
