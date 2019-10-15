5f501ee6aa004b851568067b80036d00a0aab4fc
Adolfo Ochagavía <github@adolfo.ochagavia.xyz>
2019-10-15 14:05:52 +0200

    Challenge 23

--- src/mersenne_twister_rng.rs
+++ src/mersenne_twister_rng.rs
@@ -15,8 +15,8 @@ const LOWER_MASK: u32 = (1 << R) - 1;
 const UPPER_MASK: u32 = !LOWER_MASK;
 
 pub struct Rng {
-    state: Vec<u32>,
-    index: usize,
+    pub state: Vec<u32>,
+    pub index: usize,
 }
 
 impl Rng {
@@ -39,17 +39,13 @@ impl Rng {
             self.twist()
         }
 
-        let mut x = self.state[self.index];
-        x ^= (x >> U) & D;
-        x ^= (x << S) & B;
-        x ^= (x << T) & C;
-        x ^= x >> L;
-
+        let x = self.state[self.index];
         self.index += 1;
-        x
+        temper(x)
     }
 
     fn twist(&mut self) {
+        println!("Twisting!");
         for i in 0..N {
             let x = (self.state[i] & UPPER_MASK) + (self.state[(i + 1) % N] & LOWER_MASK);
             let mut x_a = x >> 1;
@@ -61,6 +57,53 @@ impl Rng {
 
         self.index = 0;
     }
+}
+
+fn temper(mut x: u32) -> u32 {
+    x ^= (x >> U) & D;
+    x ^= (x << S) & B;
+    x ^= (x << T) & C;
+    x ^= x >> L;
+    x
+}
+
+pub fn untemper(mut x: u32) -> u32 {
+    x = un_bitshift_right_xor(x, L);
+    x = un_bitshift_left_xor(x, T, C);
+    x = un_bitshift_left_xor(x, S, B);
+    x = un_bitshift_right_xor(x, U);
+    x
+}
+
+// Adapted from https://jazzy.id.au/2010/09/22/cracking_random_number_generators_part_3.html
+fn un_bitshift_right_xor(mut value: u32, shift: u32) -> u32 {
+  let mut i = 0;
+  let mut result = 0;
+
+  while i * shift < 32 {
+    let part_mask = (0xFFFF_FFFF << (32 - shift)) >> (shift * i);
+    let part = value & part_mask;
+    value ^= part >> shift;
+    result |= part;
+    i += 1;
+  }
+
+  result
+}
+
+// Adapted from https://jazzy.id.au/2010/09/22/cracking_random_number_generators_part_3.html
+fn un_bitshift_left_xor(mut value: u32, shift: u32, mask: u32) -> u32 {
+  let mut i = 0;
+  let mut result = 0;
+
+  while i * shift < 32 {
+    let part_mask = (0xFFFF_FFFF >> (32 - shift)) << (shift * i);
+    let part = value & part_mask;
+    value ^= (part << shift) & mask;
+    result |= part;
+    i += 1;
+  }
+  return result;
 }
 
 #[test]
@@ -90,4 +133,18 @@ fn test_rng_crack_seed() {
     }
 
     assert!(found);
+}
+
+#[test]
+fn test_rng_clone() {
+    let mut rng = Rng::new(42);
+    let values: Vec<_> = (0..624).map(|_| rng.next()).collect();
+
+    let reconstructed_state: Vec<_> = values.iter().map(|&x| untemper(x)).collect();
+    let mut cloned = Rng { state: reconstructed_state, index: 0 };
+
+    let cloned_values = (0..624).map(|_| cloned.next());
+    for (cloned_value, original_value) in cloned_values.zip(values) {
+        assert_eq!(cloned_value, original_value);
+    }
 }
